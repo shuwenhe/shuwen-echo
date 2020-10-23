eba86aa56744671103f25a88efdcd99bade08da1
shuwen he <645996795@qq.com>
2020-10-23 10:22:47 +0800

    feat: NewErrIpt

---
+++ controllers/userhandler.go
@@ -0,0 +1,21 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/shuwenhe/shuwen-echo/dao"
+	utils "github.com/shuwenhe/utils"
+)
+
+func Login(ctx echo.Context) error {
+	num := ctx.FormValue("num")
+	if num == "" {
+		return ctx.JSON(utils.NewErrIpt("num is not null"))
+	}
+	user, err := dao.Login(num)
+	if err != nil {
+		return ctx.JSON(http.StatusInternalServerError, err)
+	}
+	return ctx.JSON(http.StatusOK, user)
+}
--- routers/router.go
+++ routers/router.go
@@ -2,13 +2,19 @@ package router
 
 import (
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 	"github.com/shuwenhe/shuwen-echo/controllers"
 	"github.com/shuwenhe/shuwen-echo/controllers/test"
 )
 
 func Run() {
-	app := echo.New()
-	app.GET("/api/class/all", controllers.Index)
-	app.GET("/api/test/index2", test.Index2)
-	app.Start(":8080")
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.GET("/api/class/all", controllers.Index)
+	e.GET("/api/test/index2", test.Index2)
+	e.GET("/user/login", controllers.Login)
+
+	e.Start(":8080")
 }
